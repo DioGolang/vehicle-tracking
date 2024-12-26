f735fcd2936f6bd779d51414390873048fe5eb65
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-12-25 22:24:50 -0300

    feat: eventub

--- golang-car-traffic-simulator/internal/eventhub.go
+++ golang-car-traffic-simulator/internal/eventhub.go
@@ -1,9 +1,39 @@
 package internal
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type EventHub struct {
 	RouteService  *RouteService
 	mongoClient   *mongo.Client
 	chDriverMoved chan *DriverMovedEvent
 }
+
+func (eh *EventHub) HandlerEvent(msg []byte) error {
+	var baseEvent struct {
+		EventName string `json:"event"`
+	}
+	err := json.Unmarshal(msg, &baseEvent)
+	if err != nil {
+		return fmt.Errorf("error unmarshal")
+	}
+
+	switch baseEvent.EventName {
+	case "RouteCreated":
+		var event RouteCreatedEvent
+		err := json.Unmarshal(msg, &event)
+		if err != nil {
+			return fmt.Errorf("error unmarshalling event: %w", err)
+		}
+	case "DeliveryStarted":
+	default:
+		return errors.New("unknown event")
+	}
+	return nil
+}
+
+//1:35
