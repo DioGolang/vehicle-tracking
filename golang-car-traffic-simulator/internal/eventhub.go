a3d976020125b64a141615474343ca9c66d4634d
dvasconcelos <di0g0vasconcelos@hotmail.com>
2024-12-26 21:51:55 -0300

    feat: eventub

--- golang-car-traffic-simulator/internal/eventhub.go
+++ golang-car-traffic-simulator/internal/eventhub.go
@@ -4,13 +4,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/segmentio/kafka-go"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type EventHub struct {
-	RouteService  *RouteService
-	mongoClient   *mongo.Client
-	chDriverMoved chan *DriverMovedEvent
+	RouteService    *RouteService
+	mongoClient     *mongo.Client
+	chDriverMoved   chan *DriverMovedEvent
+	freightWriter   *kafka.Writer
+	simulatorWriter *kafka.Writer
 }
 
 func (eh *EventHub) HandlerEvent(msg []byte) error {
@@ -29,11 +32,33 @@ func (eh *EventHub) HandlerEvent(msg []byte) error {
 		if err != nil {
 			return fmt.Errorf("error unmarshalling event: %w", err)
 		}
+		return eh.handleRouteCreated(event)
 	case "DeliveryStarted":
+		var event DeliveryStartedEvent
+		err := json.Unmarshal(msg, &event)
+		if err != nil {
+			return fmt.Errorf("error unmarshalling event: %w", err)
+		}
 	default:
 		return errors.New("unknown event")
 	}
 	return nil
 }
 
-//1:35
+func (eh *EventHub) handleRouteCreated(event RouteCreatedEvent) error {
+	freightCalculatedEvent, err := RouteCreatedHandler(&event, eh.RouteService)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (eh *EventHub) handleDeliveryStartedEvent(event DeliveryStartedEvent) error {
+	err := DeliveryStartedHandler(&event, eh.RouteService, eh.chDriverMoved)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//1:44
